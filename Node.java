8ff9c8370d64416cd050ba25cec157118a280fb2
Alex Clement <aclement@kentdenver.org>
2016-09-13 21:23:28 -0600

    Update Node

--- Node.java
+++ Node.java
@@ -1,10 +1,10 @@
 /**
- * Created by aclement on 8/30/16.
+ * Created by Alex Clement on 8/29/2016.
  */
 public class Node<E> {
 
     private E item;
-    private Node next;
+    private Node<E> next;
 
     public Node(E item, Node next) {
         this.item = item;
@@ -15,7 +15,6 @@ public class Node<E> {
         this.item = item;
         this.next = null;
     }
-
 
     public E getItem() {
         return item;
@@ -31,5 +30,23 @@ public class Node<E> {
 
     public void setNext(Node next) {
         this.next = next;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node<?> node = (Node<?>) o;
+
+        if (item != null ? !item.equals(node.item) : node.item != null) return false;
+        return next != null ? next.equals(node.next) : node.next == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = item != null ? item.hashCode() : 0;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        return result;
     }
 }
